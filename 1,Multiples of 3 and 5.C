b144a5aa7ef70e0d9f912321e0c6b5a0024c1d24
JyotsnaSatvika <68173832+JyotsnaSatvika@users.noreply.github.com>
2020-10-12 19:08:40 +0530

    Added main body

--- 1,Multiples of 3 and 5.C
+++ 1,Multiples of 3 and 5.C
@@ -2,5 +2,13 @@
 #include<conio.h>
 void main()
 {
-
+    int sum=0;
+    for(int i=999;i>=0;i--)
+    {
+        if(i%3==0)
+            sum=sum+i;
+        else if(i%5==0)
+            sum=sum+i;
+    }
+    printf("sum is %d",sum);
 }
