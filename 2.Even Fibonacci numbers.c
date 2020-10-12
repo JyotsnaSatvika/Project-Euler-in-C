e2d80a98803bcdc9f3b872fcc7960cd8aef362b9
JyotsnaSatvika <68173832+JyotsnaSatvika@users.noreply.github.com>
2020-10-12 19:58:22 +0530

    Create 2.Fibonacci numbers

---
+++ 2.Even Fibonacci numbers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int first_previous=2;
+    int second_previous=1;
+    long int limit=4000000;
+    long int sum=2;
+    long int fibo=0;
+    while(fibo<=limit)
+    {   
+        fibo=first_previous+second_previous;
+        second_previous=first_previous;
+        first_previous=fibo;
+        if(fibo%2==0)
+        {
+         sum=sum+fibo;  
+        }
+    }
+    printf("%ld",sum);
+}
